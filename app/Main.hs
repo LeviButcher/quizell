3547393606ffc8d37d3a0f3826ea535275bf1cb8
Levi Butcher <levibutcher355@gmail.com>
2022-03-14 00:50:04 -0400

    Kills quiz after time limit

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
 import Brick (defaultMain)
+import Control.Concurrent (killThread, threadDelay)
+import Control.Concurrent.Async (race)
+import Control.Exception (Exception, throw)
 import qualified Control.Monad as Data.Foldable
 import Data.Functor ((<&>))
 import qualified Data.Functor
@@ -42,7 +45,8 @@ data ProgramArgs = ProgramArgs
   { quizPath :: String,
     quizLength :: Int,
     tuiOn :: Bool,
-    showResults :: Bool
+    showResults :: Bool,
+    time :: Int
   }
 
 parseArgs :: Parser ProgramArgs
@@ -50,8 +54,9 @@ parseArgs =
   ProgramArgs
     <$> strOption (long "file" <> short 'f' <> metavar "Quiz File Path" <> help "Full or Relative path to Quiz file")
     <*> option auto (long "length" <> short 'l' <> help "Number of questions to use" <> metavar "INT" <> value 0)
-    <*> switch (long "tui" <> short 't' <> help "Turn on TUI mode (Works only on Unix)")
+    <*> switch (long "tui" <> help "Turn on TUI mode (Works only on Unix)")
     <*> switch (long "results" <> short 'r' <> help "Show your past quiz results")
+    <*> option auto (long "time" <> short 't' <> help "Amount of time for quiz (In Seconds)" <> metavar "INT" <> value 0)
 
 main :: IO ()
 main = runProgram =<< execParser opts
@@ -64,20 +69,37 @@ main = runProgram =<< execParser opts
             <> header "quizell"
         )
 
+data QuizException = OutOfTime deriving (Show)
+
+instance Exception QuizException
+
+secondsToMicro x = x * 1000000
+
+quizStopper :: Int -> IO ()
+quizStopper n = do
+  threadDelay . secondsToMicro $ n
+  throw OutOfTime
+
 runProgram :: ProgramArgs -> IO ()
-runProgram (ProgramArgs q l t r) = do
-  if r
-    then printUserLogs getLoginName
-    else
-      ( do
-          qList <- getQuestionList q l
-          case qList of
-            Left err -> putStrLn err
-            Right quiz -> do
-              let prog = if t then tuiApp else normalApp
-              res <- prog q getLoginName quiz
-              Data.Foldable.forM_ res toLog
-      )
+runProgram (ProgramArgs q l t r time) = do
+  res <- race (quizStopper time) actualProgram
+  case res of
+    Left _ -> putStrLn "TIMES UP, TRY NEXT TIME KID"
+    Right _ -> putStrLn "Thank you for using quizell"
+  where
+    actualProgram = do
+      if r
+        then printUserLogs getLoginName
+        else
+          ( do
+              qList <- getQuestionList q l
+              case qList of
+                Left err -> putStrLn err
+                Right quiz -> do
+                  let prog = if t then tuiApp else normalApp
+                  res <- prog q getLoginName quiz
+                  Data.Foldable.forM_ res toLog
+          )
 
 tuiApp :: String -> IO String -> Q.QuestionList -> IO (Maybe QR.QuizResults)
 tuiApp q getUser questionList = do
